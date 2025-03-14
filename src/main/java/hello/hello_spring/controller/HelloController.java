e5634dd0760560ff2f30c38d22c89ce24a8af0a3
ZANDHIFORCE <91401488+ZANDHIFORCE@users.noreply.github.com>
2025-03-14 23:47:27 +0900

    API

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +20,34 @@ public class HelloController {
         model.addAttribute("name",name);
         return "hello-template";
     }
+
+    // 잘 안씀
+    @GetMapping("hello-string")
+    @ResponseBody // http 바디에 통으로 넣겠다 -> view 이런거 안 거치고 문자가 그대로 내려감
+    public String helloString(@RequestParam(value = "name", required = false) String name){
+        return "hello " + name; // hello spring
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(name="name",required = false) String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //객체반환은 기본으로 JSON으로 가진다
+    }
+
+    // 잘 씀 -> 데이터 요청
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+    }
+
+
 }
